d1d422c06a0b1a413476c9a979dc9fa5abc8a01f
Ildikó Mag Mokus Horváth <horvath.ildiko.mm@gmail.com>
2017-11-09 14:46:24 +0100

    Add makeGrid function

--- designs.js
+++ designs.js
@@ -3,8 +3,13 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(h,w) {
+  for(let i = 0 ; i < h; ++i){
+    $('#pixel_canvas').append("<tr> </tr>")
+  }
+  for(let j = 0 ; j < w ; ++j){
+  $('tr').append("<td> </td>")
+  }
+}
 
 // Your code goes here!
-
-}
