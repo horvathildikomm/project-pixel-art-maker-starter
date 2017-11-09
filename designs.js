228262650fd105622e1592033b56be1500aeed7d
Ildikó Mag Mokus Horváth <horvath.ildiko.mm@gmail.com>
2017-11-09 16:09:08 +0100

    Add click and submit

--- designs.js
+++ designs.js
@@ -2,14 +2,26 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+makeGrid(10,10);
 
 function makeGrid(h,w) {
+  $('#pixel_canvas').empty();
   for(let i = 0 ; i < h; ++i){
     $('#pixel_canvas').append("<tr> </tr>")
   }
   for(let j = 0 ; j < w ; ++j){
   $('tr').append("<td> </td>")
   }
+
+  $('td').click(function(){
+    const color = $('#colorPicker').val();
+    $(this).css("background-color", color);
+  });
 }
 
-// Your code goes here!
+$('#sizePicker').submit(function(e){
+  e.preventDefault();
+  const h = $('#input_height').val();
+  const w = $('#input_width').val();
+  makeGrid(h,w);
+});
